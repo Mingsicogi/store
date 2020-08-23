921c001c95de341d22c2dc770b6322e8235dd962
Mingsicogi <codedoctor119@gmail.com>
2020-08-24 00:26:03 +0900

    Initial commit

---
+++ src/test/java/mins/study/store/StoreApplicationTests.java
@@ -0,0 +1,14 @@
+package mins.study.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
