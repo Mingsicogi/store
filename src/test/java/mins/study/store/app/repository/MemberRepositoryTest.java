849041e17f2accd9900b2c5d7aa0e1115e0069eb
Mingsicogi <codedoctor119@gmail.com>
2020-08-26 03:14:37 +0900

    + db configuration add

--- src/test/java/mins/study/store/app/repository/MemberRepositoryTest.java
+++ src/test/java/mins/study/store/app/repository/MemberRepositoryTest.java
@@ -1,21 +1,14 @@
 package mins.study.store.app.repository;
 
-import mins.study.store.app.entity.Member;
+import mins.study.store.app.domain.Member;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import javax.persistence.EntityManager;
 
 @DataJpaTest
 @EnableAutoConfiguration
@@ -30,12 +23,12 @@ class MemberRepositoryTest {
     @Rollback(false)
     void save() {
         Member member = new Member();
-        member.setUsername("A");
+        member.setName("A");
 
         Long memberId = memberRepository.save(member);
 
         Member dbInfo = memberRepository.find(memberId);
-        Assertions.assertEquals(member.getUsername(), dbInfo.getUsername());
+        Assertions.assertEquals(member.getName(), dbInfo.getName());
     }
 
     @Test
