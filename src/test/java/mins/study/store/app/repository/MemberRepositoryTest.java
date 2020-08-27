7382e7c51effdfa49324bd5516cbddb459884b2e
Mingsicogi <codedoctor119@gmail.com>
2020-08-28 02:31:45 +0900

    + Member repository implements

--- src/test/java/mins/study/store/app/repository/MemberRepositoryTest.java
+++ src/test/java/mins/study/store/app/repository/MemberRepositoryTest.java
@@ -1,13 +1,9 @@
 package mins.study.store.app.repository;
 
-import mins.study.store.app.domain.Member;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 
 @DataJpaTest
@@ -19,19 +15,5 @@ class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
-    @Test
-    @Rollback(false)
-    void save() {
-        Member member = new Member();
-        member.setName("A");
 
-        Long memberId = memberRepository.save(member);
-
-        Member dbInfo = memberRepository.find(memberId);
-        Assertions.assertEquals(member.getName(), dbInfo.getName());
-    }
-
-    @Test
-    void find() {
-    }
 }
