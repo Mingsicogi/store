e94dd9c24126ce3648721a08a55a61105126e744
Mingsicogi <codedoctor119@gmail.com>
2020-08-29 02:20:50 +0900

    + Item service implements

---
+++ src/main/java/mins/study/store/app/service/ItemService.java
@@ -0,0 +1,30 @@
+package mins.study.store.app.service;
+
+import lombok.RequiredArgsConstructor;
+import mins.study.store.app.domain.item.Item;
+import mins.study.store.app.repository.ItemRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id) {
+        return itemRepository.findOne(id);
+    }
+}
