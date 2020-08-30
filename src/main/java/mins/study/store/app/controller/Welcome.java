017c6dd6721069d8eadcee46d5cb10083b3a3ab6
Mingsicogi <codedoctor119@gmail.com>
2020-08-30 23:31:05 +0900

    + Querydsl repository implement and make test code

--- src/main/java/mins/study/store/app/controller/Welcome.java
+++ src/main/java/mins/study/store/app/controller/Welcome.java
@@ -1,8 +1,14 @@
 package mins.study.store.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Welcome {
 
+    @RequestMapping("/")
+    public String home() {
+
+        return "home";
+    }
 }
